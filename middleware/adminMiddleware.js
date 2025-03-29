ba75de9602f9b0fc2dd339de850ea9858e7db35b
raghvendra7100 <sharmaraghvendra63@gmail.com>
2025-03-29 23:28:39 +0530

    adding the auth middleware for admin

---
+++ middleware/adminMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const {JWT_ADMIN_SECRET} = require("../config");
+
+function adminMiddleware(req , res , next){
+const token   = req.headers.token;
+const decodedIno  = token.verify(token , JWT_ADMIN_SECRET);
+ 
+    if(decodedIno){
+        const adminId = decodedIno.id;
+        next()
+    }else{
+        res.status(403).json({
+            message: "you are not signed in ";
+        })
+    }
+}
+
+module.exports ={
+    adminMiddleware: adminMiddleware
+}
