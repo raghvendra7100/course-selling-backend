e10220733c1544aea3eb2dd7221e25cab3e7863a
raghvendra7100 <sharmaraghvendra63@gmail.com>
2025-03-31 14:51:57 +0530

    completed the enpoints of course.js

--- routes/course.js
+++ routes/course.js
@@ -1,19 +1,37 @@
-const { Router } = require("express")
-
+const { Router } = require("express");
+const { purcahseModel } = require("../database/db");
+const {userMiddleware} = require("../middleware/userMiddleware")
 const courseRouter = Router();
 
-courseRouter.get("/preview"  , (req,res)=>{
-    res.json({
-        message: "working routes"
+courseRouter.post("/purchases" , async(req, res)=>{
+    const {userId , courseId} =req.body;
+    const alreadyBought = await purcahseModel.findOne({
+        userId,
+        courseId
     })
-})
+    if(alreadyBought){
+        res.json({message : "course already purchased ! "})
+        return
+    }
+    const purchase  = await purcahseModel.create({
+        userId,
+        courseId
+    })
 
-courseRouter.get("/purchases" , (req, res)=>{
     res.json({
-        message: "working routes"
+        message: "You are now successfully enrolled",
+        purchaseId: purchase._id
     })
+
+
 })    
 
+courseRouter.get("/preview"  , async(req,res)=>{
+    const course = await courseModel.find({});
+    res.json({
+        course
+    })
+})
 module.exports ={  
     courseRouter:courseRouter
 }
